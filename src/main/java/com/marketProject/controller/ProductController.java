99b1f545e98be4d4e14007a41c554edc3c26ab0b
nhs219 <heesun_z@naver.com>
2022-05-24 14:02:20 +0900

    chore(Product) - Product service 구현

---
+++ src/main/java/com/marketProject/controller/ProductController.java
@@ -0,0 +1,36 @@
+package com.marketProject.controller;
+
+import com.marketProject.jpa.entity.Product;
+import com.marketProject.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequestMapping("/shop/products")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    // 추후에 카테고리별 조회 기능 추가해야함.
+//    @GetMapping
+//    @ResponseBody
+//    public List<Product> findProducts(@RequestParam Map<String, Object> reqParam) {
+//        return productService.findProducts(reqParam);
+//    }
+
+    @GetMapping
+    @ResponseBody
+    public List<Product> findProducts() {
+        return productService.findProducts();
+    }
+
+    @GetMapping("/{productCode}")
+    @ResponseBody
+    public Product findProduct(@PathVariable("productCode") String productCode) {
+        return productService.findProduct(productCode);
+    }
+}
