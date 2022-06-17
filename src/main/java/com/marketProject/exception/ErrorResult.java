9546489e4a27fe6cefe6d9affcb379cf387a5676
nhs219 <heesun4947@gmail.com>
2022-06-17 23:01:12 +0900

    feat: ErrorResult 구현

--- src/main/java/com/marketProject/exception/ErrorResult.java
+++ src/main/java/com/marketProject/exception/ErrorResult.java
@@ -1,2 +1,19 @@
-package com.marketProject.exception;public class ErrorResult {
+package com.marketProject.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.net.URI;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class ErrorResult {
+    LocalDateTime timestamp;
+    int status;
+    String errors;
+
 }
