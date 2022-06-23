956d97279049d3b483564a32c0edf52e103ac0f2
nhs219 <heesun4947@gmail.com>
2022-06-23 22:35:02 +0900

    feat: querydsl 적용

--- src/main/java/com/marketProject/jpa/repository/OrderRepository.java
+++ src/main/java/com/marketProject/jpa/repository/OrderRepository.java
@@ -1,8 +1,15 @@
 package com.marketProject.jpa.repository;
 
+import com.marketProject.domain.member.OrderStatus;
 import com.marketProject.jpa.entity.Order;
+import com.marketProject.jpa.entity.QMember;
+import com.marketProject.jpa.entity.QOrder;
+import com.marketProject.service.OrderServiceImpl;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import java.util.List;
@@ -23,13 +30,32 @@ public class OrderRepository {
 
     // 동적 쿼리의 지옥.... jpa criteria도 헬임
     // querydsl 공부가 필요함
+
     public List<Order> findAll(OrderSearch orderSearch) {
-       return em.createQuery("select o from Order o join o.member m" +
-                "where o.status = :status" +
-                "and m.email like :email", Order.class)
-                .setParameter("status", orderSearch.getOrderStatus())
-                .setParameter("email", orderSearch.getMemberEmail())
-                .setMaxResults(1000) // 최대 1000건
-                .getResultList();
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
+
+        return query
+                .select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()), emailLike(orderSearch.getMemberEmail()))
+                .limit(1000)
+                .fetch();
+    }
+
+    private BooleanExpression emailLike(String memberEmail) {
+        if (!StringUtils.hasText(memberEmail)) {
+            return null;
+        }
+        return QMember.member.email.like(memberEmail);
+    }
+
+    private BooleanExpression statusEq(OrderStatus statusCond) {
+        if (statusCond == null) {
+            return null;
+        }
+        return QOrder.order.orderStatus.eq(statusCond);
     }
 }
